ddc0112dfac1263fe5795a76a5e3ca317a3fd12a
afonso <afonso@tabler-mac-9.mynet>
2025-04-18 12:52:59 +0100

    Initial setup for GitHub Pages: Added Jekyll configuration, updated layout, and reorganized files

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.4.1"
+gem "webrick", "~> 1.8"
+gem "bundler", "~> 2.6.8"
+gem "github-pages", "~> 228", group: :jekyll_plugins 
---
+++ js/main.js
@@ -0,0 +1,11 @@
+var app = angular.module('myApp', []);
+
+app.controller('MainCtrl', function($scope) {
+    // You can add dynamic content here later
+    $scope.posts = [];
+    $scope.news = [];
+    $scope.publications = [];
+    $scope.media_coverage = [];
+    $scope.supervision = [];
+    $scope.awards = [];
+}); 
